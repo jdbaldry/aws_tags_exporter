f04009fc291506af36a9d848f9ecf2a2640ed42c
jdb <jdbaldry@gmail.com>
2018-07-17 13:04:42 +0100

    Vendoring

--- collector/collector.go
+++ collector/collector.go
@@ -4,7 +4,10 @@ import (
 	"regexp"
 	"sync"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -39,11 +42,15 @@ var (
 // AvailableCollectors is a map of all implemented collectors with the associated
 // registration function
 var AvailableCollectors = map[string]func(registry prometheus.Registerer, region string){
-	"ec2":   RegisterEC2Collector,
-	"efs":   RegisterEFSCollector,
-	"elb":   RegisterELBCollector,
-	"elbv2": RegisterELBV2Collector,
-	"rds":   RegisterRDSCollector,
+	"autoscaling": RegisterAutoscalingCollector,
+	"dynamodb":    RegisterDynamoDBCollector,
+	"ec2":         RegisterEC2Collector,
+	"efs":         RegisterEFSCollector,
+	"elasticache": RegisterElasticacheCollector,
+	"elb":         RegisterELBCollector,
+	"elbv2":       RegisterELBV2Collector,
+	"rds":         RegisterRDSCollector,
+	"route53":     RegisterRoute53Collector,
 }
 
 func makeConcurrentRequests(reqs []*request.Request, service string) []error {
@@ -59,6 +66,19 @@ func makeConcurrentRequests(reqs []*request.Request, service string) []error {
 	}
 	wg.Wait()
 	return errs
+}
+
+func getAccountID() (string, error) {
+	st := sts.New(session.New(&aws.Config{}))
+	out, err := st.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+
+	RequestTotalMetric.With(prometheus.Labels{"service": "sts", "region": "global"}).Inc()
+	if err != nil {
+		RequestErrorTotalMetric.With(prometheus.Labels{"service": "sts", "region": "global"}).Inc()
+		return "", err
+	}
+
+	return *out.Account, nil
 }
 
 func sanitizeLabelName(s string) string {
